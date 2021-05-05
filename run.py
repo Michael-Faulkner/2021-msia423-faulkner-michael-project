33b5228aadde2dd6724bdc1a047d6bd03e50f936
Michael Faulkner <michaelfaulkner2021@u.northwestern.edu>
2021-05-05 16:23:01 -0500

    renamed function call

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import argparse
 
 from src.get_data import download, upload
-from src.createTable import createTable
+from src.create_db import create_db
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 
 if __name__ == '__main__':
@@ -19,14 +19,14 @@ if __name__ == '__main__':
     sp_data = subparsers.add_parser("get_data", description="Downloads or Uploads data from the internet or S3")
     sp_data.add_argument("--upload", default=True, help="")
     sp_data.add_argument("--url", default="http://deepx.ucsd.edu/public/jmcauley/steam/australian_users_items.json.gz", help="url to acquire data from")
-    sp_data.add_argument("--gzip_file_path", default="data/australian_users_items.json.gz", help="Local File path to save data file")
+    sp_data.add_argument("--gzip_file_path", default="data/raw/australian_users_items.json.gz", help="Local File path to save data file")
     sp_data.add_argument("--bucket_name", default="2021-msia423-faulkner-michael", help="s3 bucket name")
     sp_data.add_argument("--bucket_path", default='data.json.gz', help="SQLAlchemy connection URI for database")
 
     args = parser.parse_args()
     sp_used = args.subparser_name
     if sp_used == 'create_db':
-        createTable(args.engine_string)
+        create_db(args.engine_string)
     elif sp_used == 'get_data':
         download(args.url, args.gzip_file_path)
         if args.upload:
