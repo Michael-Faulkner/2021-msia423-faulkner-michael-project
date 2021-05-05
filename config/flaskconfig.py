a5c92cb6c0a66ddb2ba505cab97c58cb43a6ce78
Michael Faulkner <michaelfaulkner2021@u.northwestern.edu>
2021-05-05 14:10:54 -0500

    renamed variable in flask config

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -2,7 +2,7 @@ import os
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 5000
-APP_NAME = "penny-lane"
+APP_NAME = "steam-recommender"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
@@ -13,13 +13,13 @@ DB_HOST = os.environ.get('MYSQL_HOST')
 DB_PORT = os.environ.get('MYSQL_PORT')
 DB_USER = os.environ.get('MYSQL_USER')
 DB_PW = os.environ.get('MYSQL_PASSWORD')
-DATABASE = os.environ.get('MYSQL_DATABASE')
+DATABASE = os.environ.get('DATABASE_NAME')
 DB_DIALECT = 'mysql+pymysql'
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 if SQLALCHEMY_DATABASE_URI is not None:
     pass
 elif DB_HOST is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/msia423_db.db'
 else:
     SQLALCHEMY_DATABASE_URI = '{dialect}://{user}:{pw}@{host}:{port}/{db}'.format(dialect=DB_DIALECT, user=DB_USER,
                                                                                   pw=DB_PW, host=DB_HOST, port=DB_PORT,
