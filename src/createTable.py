6f89f826ec5fb661da39f55507edb336e45ca19d
Michael Faulkner <michaelfaulkner2021@u.northwestern.edu>
2021-05-05 14:10:40 -0500

    updated createtable funciton to incldue any kind of engine string and to accept variables from flask config

--- src/createTable.py
+++ src/createTable.py
@@ -1,24 +1,15 @@
+import argparse
 import logging
-import pandas as pd
-import os
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
 import sqlalchemy
+from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 
 
-def createTable():
-    conn_type = "mysql+pymysql"
-    user = os.environ.get("MYSQL_USER")
-    password = os.environ.get("MYSQL_PASSWORD")
-    host = os.environ.get("MYSQL_HOST")
-    port = os.environ.get("MYSQL_PORT")
-    database = os.environ.get("DATABASE_NAME")
-    engine_string = "{}://{}:{}@{}:{}/{}".format(conn_type, user, password, host, port, database)
+def createTable(engine_string):
+    base = declarative_base()
 
-    Base = declarative_base(engine_string)
-
-    class User(Base):
+    class User(base):
         """Create a data model for the database to be set up for capturing songs """
         __tablename__ = 'Users'
         id = Column(Integer, primary_key=True)
@@ -27,7 +18,7 @@ def createTable():
         def __repr__(self):
             return '<steam_id %r>' % self.steam_id
 
-    class Games(Base):
+    class Games(base):
         """Create a data model for the database to be set up for capturing songs """
         __tablename__ = 'Games'
         id = Column(Integer, primary_key=True)
@@ -37,18 +28,18 @@ def createTable():
         def __repr__(self):
             return '<game %r>' % self.game_title
 
-    class OwnedGames(Base):
+    class OwnedGames(base):
         """Create a data model for the database to be set up for capturing songs """
         __tablename__ = 'OwnedGames'
         id = Column(Integer, primary_key=True)
         steam_id = Column(String(25), unique=False, nullable=False)
         game_id = Column(String(100), unique=False, nullable=False)
 
-    # set up sqlite connection
-
     engine = sqlalchemy.create_engine(engine_string)
-    # create the tracks table
-    Base.metadata.create_all(engine)
+    base.metadata.create_all(engine)
 
 
-createTable()
+
+
+
+
